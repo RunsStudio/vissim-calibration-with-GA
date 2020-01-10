76450e98709669723ba08818b45cf4c41594b16b
RunsStudio <332471342@qq.com>
2020-01-10 11:55:21 +0800

    更新

--- MyProblem.py
+++ MyProblem.py
@@ -6,10 +6,7 @@ import win32com.client as com  # VISSIM COM
 import time
 
 """
-该案例展示了一个简单的连续型决策变量最大化目标的单目标优化问题。
-max f = x * np.sin(10 * np.pi * x) + 2.0
-s.t.
--1 <= x <= 2
+
 """
 
 
@@ -22,15 +19,15 @@ class MyProblem(ea.Problem):  # 继承Problem父类
         maxormins = [1]  # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
         Dim = 3  # 初始化Dim（决策变量维数）
         varTypes = [0] * Dim  # 初始化varTypes（决策变量的类型，元素为0表示对应的变量是连续的；1表示是离散的）
-        lb = [1.2, 1.0, 2]  # 决策变量下界 CC0推荐：1.2-1.7
-        ub = [1.7, 2.0, 7]  # 决策变量上界
+        lb = [1.2, 0.9, 2]  # 决策变量下界 CC0推荐：1.2-1.7
+        ub = [1.7, 1.8, 7]  # 决策变量上界
         lbin = [1, 1, 1]  # 决策变量下边界
         ubin = [1, 1, 1]  # 决策变量上边界
         # 调用父类构造方法完成实例化
         ea.Problem.__init__(self, name, M, maxormins, Dim, varTypes, lb, ub, lbin, ubin)
 
     def aimFunc(self, pop):  # 目标函数
-        x = pop.Phen  # 得到决策变量矩阵 x=50
+        x = pop.Phen  # 得到决策变量矩阵 x=30
         x0 = x[:, [0]]
         x1 = x[:, [1]]
         x2 = x[:, [2]]
@@ -45,6 +42,10 @@ class MyProblem(ea.Problem):  # 继承Problem父类
             self.V2 = self.Table.col_values(1)  # spd for segment 2
             self.V3 = self.Table.col_values(2)  # spd for segment 2
             self.V4 = self.Table.col_values(3)  # spd for segment 2
+            self.V5 = self.Table.col_values(6)  # spd for segment 2
+            self.V6 = self.Table.col_values(7)  # spd for segment 2
+            self.V7 = self.Table.col_values(8)  # spd for segment 2
+            self.V8 = self.Table.col_values(9)  # spd for segment 2
             self.relativeFlow = self.Table.col_values(4)  # relative flow
             self.volume = self.Table.col_values(5)  # 流量
         totalResult = []
@@ -65,7 +66,7 @@ class MyProblem(ea.Problem):  # 继承Problem父类
             dbpss = self.Net.DrivingBehaviorParSets  # Driving behavior module
             dbps = dbpss(3)
             # # Set Simulation Parameters
-            TotalPeriod = 82802  # Define total simulation period
+            TotalPeriod = 55802  # Define total simulation period
             WarmPeriod = 900  # Define warm period 10 minutes
             Random_Seed = k  # Define random seed
             step_time = 1  # Define Step Time
@@ -81,6 +82,10 @@ class MyProblem(ea.Problem):  # 继承Problem父类
             t2 = []
             t3 = []
             t4 = []
+            t5 = []
+            t6 = []
+            t7 = []
+            t8 = []
             nVeh = []
             dbps.SetAttValue('CC0', x0[k][0])  ## 天坑！ 不能写x0[k]！！！！！！
             dbps.SetAttValue('CC1', x1[k][0])
@@ -96,30 +101,45 @@ class MyProblem(ea.Problem):  # 继承Problem父类
             dt2 = dataCollections(2)
             dt3 = dataCollections(3)
             dt4 = dataCollections(4)
+            dt5 = dataCollections(5)
+            dt6 = dataCollections(6)
+            dt7 = dataCollections(7)
+            dt8 = dataCollections(8)
             composition = self.Net.TrafficCompositions(1)
             vehicleInput = self.Net.VehicleInputs(1)
             # self.Sim.RunContinuous()
             for j in range(1, TotalPeriod):
                 if (j % 900 == 0) and (j >= 1801):
-                    composition.SetAttValue1("RELATIVEFLOW", 100, 1 - self.relativeFlow[int(j / 900-3)])
-                    composition.SetAttValue1("RELATIVEFLOW", 200, self.relativeFlow[int(j / 900-3)])
-                    vehicleInput.SetAttValue("VOLUME", self.volume[int(j / 900-3)])
+                    composition.SetAttValue1("RELATIVEFLOW", 100, 1 - self.relativeFlow[int(j / 900 - 3)])
+                    composition.SetAttValue1("RELATIVEFLOW", 200, self.relativeFlow[int(j / 900 - 3)])
+                    vehicleInput.SetAttValue("VOLUME", self.volume[int(j / 900 - 3)])
                     t1.append(dt1.GetResult("speed", "mean", 0))  # 车道1
                     t2.append(dt2.GetResult("speed", "mean", 0))  # 车道2
                     t3.append(dt3.GetResult("speed", "mean", 0))  # 车道3
                     t4.append(dt4.GetResult("speed", "mean", 0))  # 车道4
+                    t5.append(dt5.GetResult("speed", "mean", 0))  # 车道1
+                    t6.append(dt6.GetResult("speed", "mean", 0))  # 车道2
+                    t7.append(dt7.GetResult("speed", "mean", 0))  # 车道3
+                    t8.append(dt8.GetResult("speed", "mean", 0))  # 车道4
                     nVeh.append(4 * (dt1.GetResult("NVEHICLES", "sum", 0)
-                                     + dt2.GetResult("NVEHICLES", "sum",0)
+                                     + dt2.GetResult("NVEHICLES", "sum", 0)
                                      + dt3.GetResult("NVEHICLES", "sum", 0)
                                      + dt4.GetResult("NVEHICLES", "sum", 0)))
                 self.Sim.RunSingleStep()
 
-            spdTotal = np.array(0.25 * sum(abs(np.array(t1) - np.array(self.V1)) / np.array(self.V1)
-                                           + abs(np.array(t2) - np.array(self.V2)) / np.array(self.V1)
-                                           + abs(np.array(t3) - np.array(self.V3)) / np.array(self.V1)
-                                           + abs(np.array(t4) - np.array(self.V4)) / np.array(self.V1)))
-            tTimeTotal = np.array(sum(abs(np.array(nVeh) - np.array(self.volume)) / np.array(self.volume)))
-            totalResult.append(spdTotal+tTimeTotal)
+            spdTotal = np.array(0.125 * sum(abs(np.array(t1) - np.array(self.V1)) / np.array(self.V1)
+                                            + abs(np.array(t2) - np.array(self.V2)) / np.array(self.V2)
+                                            + abs(np.array(t3) - np.array(self.V3)) / np.array(self.V3)
+                                            + abs(np.array(t4) - np.array(self.V4)) / np.array(self.V4)
+                                            + abs(np.array(t5) - np.array(self.V5)) / np.array(self.V5)
+                                            + abs(np.array(t6) - np.array(self.V6)) / np.array(self.V6)
+                                            + abs(np.array(t7) - np.array(self.V7)) / np.array(self.V7)
+                                            + abs(np.array(t8) - np.array(self.V8)) / np.array(self.V8)))
+            NVehTotal = np.array(
+                sum(abs(np.array(nVeh) - np.array(self.volume)) / np.array(self.volume)))  ## 这里应该是通过的车辆数（变量名起错了）
+            totalResult.append(spdTotal + NVehTotal)
+            print("spd误差：", spdTotal)
+            print("NVeh误差：", NVehTotal)
             print("总误差为：", totalResult[k])
             self.Sim.Stop()
         pop.ObjV = np.vstack(totalResult)  # 计算目标函数值，赋值给pop种群对象的ObjV属性
